2ea61153778538b1fb7035d2ff029f3b55873a52
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-03-16 19:00:12 -0300

    feat: do not create a voucher if already exists test

--- tests/unit/voucherService.spec.ts
+++ tests/unit/voucherService.spec.ts
@@ -1,10 +1,12 @@
 import { jest } from "@jest/globals";
 import voucherService from "services/voucherService";
 import voucherRepository from "repositories/voucherRepository";
+import { conflictError } from "utils/errorUtils";
 
-describe("Create voucher test suite", () => {
+describe("create voucher test suite", () => {
   it("should be able to create a voucher succesfully", async () => {
     const voucher = {
+      id: 1,
       code: "sup3r1d0l",
       discount: 30,
       used: false,
@@ -23,5 +25,24 @@ describe("Create voucher test suite", () => {
     await voucherService.createVoucher(voucher.code, voucher.discount);
 
     expect(Promise.resolve);
+  });
+
+  it("should not be able to create a voucher that already exists", () => {
+    expect(async () => {
+      const voucher = {
+        id: 1,
+        code: "sup3r1d0l",
+        discount: 30,
+        used: false,
+      };
+
+      jest
+        .spyOn(voucherRepository, "getVoucherByCode")
+        .mockImplementationOnce((): any => {
+          return voucher;
+        });
+
+      await voucherService.createVoucher(voucher.code, voucher.discount);
+    }).rejects.toMatchObject(conflictError("Voucher already exist."));
   });
 });
